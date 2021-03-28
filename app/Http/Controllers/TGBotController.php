515f45f10abd1fbf38e339f431e522bbcbd90698
saidjon <saidjons@bk.ru>
2021-03-28 14:07:54 +0500

    nuts

--- app/Http/Controllers/TGBotController.php
+++ app/Http/Controllers/TGBotController.php
@@ -34,7 +34,7 @@ class TGBotController extends Controller
         // this is ntgram  
 
         $bot = new Nutgram($_ENV['TELEGRAM_TOKEN']); // new instance
-            $bot->setRunningMode(Webhook::class);
+            $bot->setRunningMode(Polling::class);
                  
                 $bot->onMessage(function (Nutgram $bot) {
                     $bot->sendMessage('You sent a message!');
