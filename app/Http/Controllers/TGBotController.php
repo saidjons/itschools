a0fc70af7b986e3422a8a951116d17a40eba5273
saidjon <saidjons@bk.ru>
2021-03-28 13:08:46 +0500

    tg onmessage

--- app/Http/Controllers/TGBotController.php
+++ app/Http/Controllers/TGBotController.php
@@ -19,20 +19,13 @@ class TGBotController extends Controller
 
         $bot = new Nutgram($_ENV['TOKEN']); // new instance
             $bot->setRunningMode(Webhook::class);
-                 $bot->onMessage('you send a message');
-        $bot->onText('My name is {name}', function (Nutgram $bot, $name) {
-            $bot->sendMessage("Hi {$name}");
-        });
+                $bot->onMessage(function (Nutgram $bot) {
+                $bot->onMessage(function (Nutgram $bot) {
+                    $bot->sendMessage('You sent a message!');
+                });
 
-        // ex. called when a message contains "I want 6 pizzas"
-        // $bot->onText('hey', function (Nutgram $bot, $n) {
-        //     $bot->sendMessage("You will get {$n} pizzas!");
-        // });
 
-        $bot->onText('hey ([0-9]+))', function (Nutgram $bot, $amount, $dish) {
-            $bot->sendMessage("You will get {$amount} portions of  !");
-        });
-
+       
             // ...
 
             $bot->run();
