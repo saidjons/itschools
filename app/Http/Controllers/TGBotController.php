1aeb8d95253b3a5e82ad4f1cb17ffecdd76ae419
saidjon <saidjons@bk.ru>
2021-03-28 16:37:58 +0500

    sdk get id

--- app/Http/Controllers/TGBotController.php
+++ app/Http/Controllers/TGBotController.php
@@ -23,7 +23,7 @@ class TGBotController extends Controller
 
         $response = Telegram::sendMessage([
         'chat_id' => '1289432718', 
-        'text' => "<pre>".json_encode($updates)."<pre>",
+        'text' => $updates->messege->from->id ?? $updates->from->id,
         ]);
     
 
