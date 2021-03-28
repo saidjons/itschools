20a8deaf9ad7d13a9843578e7cc57ea8cf837ef4
saidjon <saidjons@bk.ru>
2021-03-28 17:15:55 +0500

    ss

--- resources/views/udema/home.blade.php
+++ resources/views/udema/home.blade.php
@@ -374,4 +374,12 @@
     </div>
     <!--/call_section-->
 </main>
+<script async src="https://telegram.org/js/telegram-widget.js?14" data-telegram-login="Itschools_bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+<script type="text/javascript">
+  function onTelegramAuth(user) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
+</script>
+
+
 @endsection
